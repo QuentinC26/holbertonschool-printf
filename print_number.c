aac45b03d24cee2394ee37101bb08a95c186dfb5
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-27 16:39:34 +0100

    modify the french commentary with the english commentary

--- print_number.c
+++ print_number.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- * print_number - Affiche un entier
- * @args: Liste des arguments variadiques
+ * print_number - print a integer
+ * @args: List of arguments
  *
- * Return: Nombre de caractères imprimés
+ * Return: Number of characters printed
  */
 int print_number(va_list args)
 {
-    int num = va_arg(args, int);
-    int count = 0, temp, digit, div = 1;
+	int num = va_arg(args, int);
+	int count = 0, temp, digit, div = 1;
 
-    if (num < 0)
-    {
-        count += _putchar('-');
-        num = -num;
-    }
+	if (num < 0)
+	{
+		count += _putchar('-');
+		num = -num;
+	}
 
-    temp = num;
-    while (temp / 10)
-    {
-        div *= 10;
-        temp /= 10;
-    }
+	temp = num;
+	while (temp / 10)
+	{
+		div *= 10;
+		temp /= 10;
+	}
 
-    while (div)
-    {
-        digit = num / div;
-        count += _putchar(digit + '0');
-        num %= div;
-        div /= 10;
-    }
+	while (div)
+	{
+		digit = num / div;
+		count += _putchar(digit + '0');
+		num %= div;
+		div /= 10;
+	}
 
-    return (count);
+	return (count);
 }
