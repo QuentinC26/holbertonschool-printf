53e7c2fe9b977797e50337de5f673e2786a8f44d
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-27 17:31:02 +0100

    add a main page in the main branch

---
+++ print_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_percent - print a characters '%'
+ * @args: List of arguments (not used)
+ *
+ * Return: Number of characters printed (1)
+ */
+
+int print_percent(va_list args)
+{
+	(void)args;
+	return (_putchar('%'));
+}
