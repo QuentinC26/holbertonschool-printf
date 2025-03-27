530b26f5bce88c1c00dab422563a5ab024dcdbb3
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-27 17:31:14 +0100

    add a main page in the main branch

---
+++ print_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_string - print the string using write
+ * @args: arguments of va_list
+ * Return: number of printed characters
+ */
+
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	int len = 0;
+
+	if (!str)
+	{
+		str = "(NULL)";
+	}
+	while (str[len])
+	{
+		len++;
+	}
+	return (write(1, str, len));
+}
