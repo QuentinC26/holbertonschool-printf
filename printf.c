aadbeddc6f92c0b55b449bed97bb1567d22f8365
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-27 16:44:12 +0100

    modify the french commentary with a english commentary

--- printf.c
+++ printf.c
@@ -1,43 +1,44 @@
 #include "main.h"
 
 /**
- * _printf - Fonction principale qui imite printf
- * @format: Chaîne de format
+ * _printf - main function which imitates printf
+ * @format: chain of format
  *
- * Return: Nombre de caractères imprimés
+ * Return: Number of printed characters
  */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int (*func)(va_list);
+	va_list args;
+	int count = 0;
+	int (*func)(va_list);
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%' && *(format + 1))
-        {
-            format++;
-            func = get_function(*format);
-            if (func)
-                count += func(args);
-            else
-            {
-                count += _putchar('%');
-                count += _putchar(*format);
-            }
-        }
-        else
-        {
-            count += _putchar(*format);
-        }
-        format++;
-    }
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1))
+		{
+			format++;
+			func = get_function(*format);
+			if (func)
+				count += func(args);
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(*format);
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
